cfbb25a2f23d9a08bf93533c5f8df91dd5b157d2
ASHISH-SAINI78021 <ashu78021@gmail.com>
2024-10-28 15:39:51 +0530

    jfdsjf

--- project/src/component/Routes/Private.jsx
+++ project/src/component/Routes/Private.jsx
@@ -2,15 +2,36 @@ import React, { useEffect, useState } from 'react'
 import { Outlet } from "react-router-dom";
 import Page from '../Page';
 import { useAuth } from '../context/auth';
+import { API } from '../../const';
 
 const Private = () => {
     const [ok, setok] = useState(false);
     const [auth , setAuth] = useAuth();
     useEffect(() => {
+      // Check if there's a token in local storage
+      const storedAuth = localStorage.getItem("auth");
+      if (storedAuth) {
+        const token = JSON.parse(storedAuth);
+        console.log("token aaya hai:", token)
+        setAuth((prev) => ({ ...prev, token }));
+      } else {
+          // If no token in local storage, check URL params
+          const params = new URLSearchParams(window.location.search);
+          console.log(params);
+          const tokenFromUrl = params.get('token');
+          console.log("token aaya hai:",tokenFromUrl);
+          if (tokenFromUrl) {
+              localStorage.setItem("auth", {token:JSON.stringify(tokenFromUrl)});
+              setAuth((prev) => ({ ...prev, token: tokenFromUrl }));
+          }
+      }
+    }, []); // Dependency array to run on mount only
+
+    useEffect(() => {
       const init = async () => {
         try {
           let response = await fetch(
-            "https://convocation-portal-2.onrender.com/api/v1/auth/get-session",
+            `${API}/api/v1/auth/get-session`,
             {
               method: "GET" ,
               headers : {
